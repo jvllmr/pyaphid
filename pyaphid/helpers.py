81f9be468f420b18e5fc1f29f3f6a059f26c4e53
Jan Vollmer <jan@vllmr.dev>
2023-03-04 14:35:52 +0100

    fix echo with line ref giving out incorrect columns

--- pyaphid/helpers.py
+++ pyaphid/helpers.py
@@ -5,4 +5,4 @@ import typer
 
 def echo_with_line_ref(filepath: str, section: ast.AST, message: str):
     filepath = filepath if filepath.startswith("/") else f"./{filepath}"
-    typer.echo(f"{filepath}:{section.lineno}:{section.col_offset}: {message}")
+    typer.echo(f"{filepath}:{section.lineno}:{section.col_offset +1}: {message}")
